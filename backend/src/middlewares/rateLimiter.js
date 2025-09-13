656c9510588095eea196f34a3f6195cc2f9be936
Abhijith P A <128230502+cap-Jacksparrow@users.noreply.github.com>
2025-09-13 10:54:22 +0530

    Update rateLimiter.js

--- backend/src/middlewares/rateLimiter.js
+++ backend/src/middlewares/rateLimiter.js
@@ -1,11 +1,12 @@
 const rateLimit = require("express-rate-limit");
+const { ipKeyGenerator } = require("express-rate-limit");
 
 // 1. Login attempts - per IP
  const loginLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 5, // limit each IP to 5 requests per windowMs
   message: "Too many login attempts. Try again after a minute.",
-keyGenerator: (req) => (req.ip ? req.ip.replace(/^::ffff:/, "") : "unknown-ip") // limit per IP
+keyGenerator:ipkeyGenerator, // limit per IP
 });
 
 // 2. General API calls - per user
@@ -13,7 +14,7 @@ const apiLimiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 100,
   message: "Too many requests. Please slow down.",
- keyGenerator: (req) => (req.ip ? req.ip.replace(/^::ffff:/, "") : "unknown-ip")// fallback to IP if no user
+ keyGenerator: ipKeyGenerator,// fallback to IP if no user
 });
 
 // 3. Portfolio update - per user
@@ -21,7 +22,7 @@ const portfolioUpdateLimiter = rateLimit({
   windowMs: 1 * 60 * 1000,
   max: 10,
   message: "Too many portfolio updates. Try again later.",
- keyGenerator: (req) => (req.ip ? req.ip.replace(/^::ffff:/, "") : "unknown-ip")
+ keyGenerator: ipKeyGenerator,
 });
 
 module.exports = {loginLimiter, apiLimiter, portfolioUpdateLimiter};

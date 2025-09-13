7ab81e321bc324db7a4556fbace0a6f4592449ad
Abhijith P A <128230502+cap-Jacksparrow@users.noreply.github.com>
2025-09-13 10:48:31 +0530

    Update authRoutes.js

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -2,10 +2,10 @@ const express = require("express");
 const validate = require("../middlewares/validate");
 const { signupSchema, loginSchema } = require("../validators/authValidator");
 const { signup, login } = require("../controllers/authController");
-const { loginlimiter } = require("../middlewares/rateLimiter");
+const { loginLimiter } = require("../middlewares/rateLimiter");
 const router = express.Router();
 
 router.post("/signup", validate(signupSchema), signup);
-router.post("/login",loginlimiter, validate(loginSchema), login);
+router.post("/login",loginLimiter, validate(loginSchema), login);
 
 module.exports = router;
